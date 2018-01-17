72c79818b119ceb449862dcfa313ea63833df2ff
sqzhang <sqzhang@thoughtworks.com>
2018-01-17 18:57:29 +0800

    [shengqi] #n/a create a solution

---
+++ Refactoring.Tests/SampleTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Refactoring.Tests
+{
+    public class SampleTests
+    {
+        [Fact]
+        public void should_return_true()
+        {
+            Assert.True(true);
+        }
+    }
+}
