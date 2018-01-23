05b2eba7a2fbfb581ea938030da0d3901a93e980
sqzhang <sqzhang@thoughtworks.com>
2018-01-23 22:34:34 +0800

    [shengqi] #n/a need to extract class.

---
+++ Refactoring.Tests/ExtractClass/CustomerGetTotalChargeFacts.cs
@@ -0,0 +1,25 @@
+﻿using Refactoring.MoveField;
+using Xunit;
+
+namespace Refactoring.Tests.MovieField
+{
+    public class CustomerGetTotalChargeFacts
+    {
+        [Fact]
+        public void should_get_correct_charge()
+        {
+            var customer = new Customer("yan");
+            customer.RentMovie("Once upon a time in America", 8.42, 2, 10);
+            Assert.Equal(26.84, customer.GetTotalCharge());
+        }
+
+        [Fact]
+        public void should_get_correct_charge_when_distance_over_10_km()
+        {
+            var customer = new Customer("yan");
+            customer.RentMovie("Gone With the Wind", 1.52, 2, 11.6, "Beijing", "Chaoyang", "Dongzhimen Street");
+            Assert.Equal(20.04, customer.GetTotalCharge());
+            Assert.Equal("Address: Dongzhimen Street, Chaoyang, Beijing", customer.GetAddress());
+        }
+    }
+}
---
+++ Refactoring/ExtractClass/Customer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Refactoring.MoveField
+{
+    public class Customer
+    {
+        public string Name { get; }
+        public int DaysRented { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+
+        public string City { get; set; }
+        public string Block { get; set; }
+        public string Street { get; set; }
+        public double Distance { get; set; }
+
+        public Customer(string name)
+        {
+            Name = name;
+        }
+
+        public void RentMovie(string tile, double price, int daysRented, double distance = 0, string city = null, string block = null, string street = null)
+        {
+            Title = tile;
+            Price = price;
+            DaysRented = daysRented;
+            Distance = distance;
+            City = city;
+            Block = block;
+            Street = street;
+        }
+
+        public double GetTotalCharge()
+        {
+            double transferFee = 0;
+
+            if (Distance > 0 && Distance <= 10)
+            {
+                transferFee = 10;
+            }
+
+            if (Distance > 10)
+            {
+                transferFee = 10 + 3.5 * (Math.Round(Distance - 10));
+            }
+            return Price * DaysRented + transferFee;
+        }
+
+        public string GetAddress()
+        {
+            return $"Address: {Street}, {Block}, {City}";
+        }
+    }
+}
