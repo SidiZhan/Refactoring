8e3112536253486c7f3944a46c812ba6d49134fb
sqzhang <sqzhang@thoughtworks.com>
2018-01-31 09:58:29 +0800

    [shengqi] #n/a generate primes.

---
+++ Refactoring.Tests/GeneratePrimes/PrimesGeneratorTests.cs
@@ -0,0 +1,30 @@
+﻿using Refactoring.GeneratePrimes;
+using Xunit;
+
+namespace Refactoring.Tests.GeneratePrimes
+{
+    public class PrimesGeneratorTests
+    {
+        [Fact]
+        public void Test_primes()
+        {
+            int[] nullArray = PrimesGenerator.GeneratePrimes(0);
+
+            Assert.Equal(0, nullArray.Length);
+
+
+            int[] minArray = PrimesGenerator.GeneratePrimes(2);
+            Assert.Equal(1, minArray.Length);
+            Assert.Equal(2, minArray[0]);
+
+            int[] threeArray = PrimesGenerator.GeneratePrimes(3);
+            Assert.Equal(2, threeArray.Length);
+            Assert.Equal(2, threeArray[0]);
+            Assert.Equal(3, threeArray[1]);
+
+            int[] centArray = PrimesGenerator.GeneratePrimes(100);
+            Assert.Equal(25, centArray.Length);
+            Assert.Equal(97, centArray[24]);
+        }
+    }
+}
---
+++ Refactoring/GeneratePrimes/PrimesGenerator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+/**
+ * This class Generates primes numbers up to a user specified maximum.
+ * The algorithm used is the Sieve of Eratosthenes.
+ * <p>
+ * Eratosthenes of Cirene, b. c. 276 BC, Cyrene, Libya --
+ * d. c. 194, Alexandria. The first man to calculate the circumference of the Earth.
+ * Also know for working on calendars with leap
+ * <p>
+ * The algorithm is quite simple. Given an array of integers starting at 2.
+ * Cross out all multiples of 2. Find the next uncrossed integer, and cross out all of its multiples.
+ * Repeat until you have passed the square root of the maximum value.
+ * @author Robert C. Martin
+ * @version 9 Dec 1999 rcm
+ * **/
+namespace Refactoring.GeneratePrimes
+{
+    public class PrimesGenerator
+    {
+        /*
+         * @param maxValue i sthe generation limit.
+        */
+        public static int[] GeneratePrimes(int maxValue)
+        {
+            if (maxValue >= 2) // the only valid case
+            {
+                //declarations
+                int s = maxValue + 1;//size of array
+                bool[] f = new bool[s];
+                int i;
+
+                //initialize array to true
+                for (i = 0; i < s; i++)
+                    f[i] = true;
+
+                //get rid of know non-primes
+                f[0] = f[1] = false;
+
+                //sieve
+                int j;
+                for ( i = 0; i < Math.Sqrt(s) + 1; i++)
+                {
+                    for (j = 0; j < s; j += i)
+                       f[j] = false;// multiple is not prime
+                }
+
+                //how many primes are there?
+                int count = 0;
+                for (i = 0; i < s; i++)
+                {
+                    if (f[i])
+                       count++;
+                }
+
+                int[] primes = new int[count];
+
+                //move the primes into the result.
+                for (i = 0, j =0; i < s; i++)
+                {
+                    if (f[i])   //if prime
+                        primes[j++] = i;
+                }
+
+                return primes; // return the prime
+            }
+            else // maxValue < 2
+            {
+                return new int[0];
+            }
+        }
+    }
+}
