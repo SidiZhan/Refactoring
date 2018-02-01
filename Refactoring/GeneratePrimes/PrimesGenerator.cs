5bca3078b3b9e319e38328f80809bcb5f38d3521
sqzhang <sqzhang@thoughtworks.com>
2018-02-01 13:28:33 +0800

    [cc] #N/A rename

--- Refactoring/GeneratePrimes/PrimesGenerator.cs
+++ Refactoring/GeneratePrimes/PrimesGenerator.cs
@@ -18,19 +18,19 @@ namespace Refactoring.GeneratePrimes
     public class PrimesGenerator
     {
         /*
-         * @param maxValue i sthe generation limit.
+         * @param maxValue is the generation limit.
         */
         public static int[] GeneratePrimes(int maxValue)
         {
             if (maxValue >= 2) // the only valid case
             {
                 //declarations
-                int s = maxValue + 1;//size of array
-                bool[] f = new bool[s];
+                int size = maxValue + 1;
+                bool[] f = new bool[size];
                 int i;
 
                 //initialize array to true
-                for (i = 0; i < s; i++)
+                for (i = 0; i < size; i++)
                     f[i] = true;
 
                 //get rid of know non-primes
@@ -38,15 +38,15 @@ namespace Refactoring.GeneratePrimes
 
                 //sieve
                 int j;
-                for ( i = 2; i < Math.Sqrt(s) + 1; i++)
+                for ( i = 2; i < Math.Sqrt(size) + 1; i++)
                 {
-                    for (j = 2* i; j < s; j += i)
+                    for (j = 2* i; j < size; j += i)
                        f[j] = false;// multiple is not prime
                 }
 
                 //how many primes are there?
                 int count = 0;
-                for (i = 0; i < s; i++)
+                for (i = 0; i < size; i++)
                 {
                     if (f[i])
                        count++; // bump count.
@@ -55,7 +55,7 @@ namespace Refactoring.GeneratePrimes
                 int[] primes = new int[count];
 
                 //move the primes into the result.
-                for (i = 0, j =0; i < s; i++)
+                for (i = 0, j =0; i < size; i++)
                 {
                     if (f[i])   //if prime
                         primes[j++] = i;
@@ -63,10 +63,7 @@ namespace Refactoring.GeneratePrimes
 
                 return primes; // return the prime
             }
-            else // maxValue < 2
-            {
-                return new int[0]; // return null array if bad input.
-            }
+            return new int[0]; // return null array if bad input.
         }
     }
 }
