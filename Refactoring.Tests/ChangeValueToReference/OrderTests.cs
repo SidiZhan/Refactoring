ab01f1689dd1ac3e2a4af62a31fa3ac83381457e
sqzhang <sqzhang@thoughtworks.com>
2018-02-03 16:56:47 +0800

    [shengqi] #n/a change value to reference.

---
+++ Refactoring.Tests/ChangeValueToReference/OrderTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Refactoring.ChangeValueToReference;
+using Xunit;
+
+namespace Refactoring.Tests.ChangeValueToReference
+{
+    public class OrderTests
+    {
+        [Fact]
+        public void get_all_report_for_one_user()
+        {
+            var orders = new List<Order>
+            {
+                new Order(new User("shengqi"), new List<Product>() {new Product("Pen", 2.5)}),
+                new Order(new User("shengqi"), new List<Product>() {new Product("Pen", 2.5)}),
+                new Order(new User("shengqi"), new List<Product>() {new Product("NoteBook", 2.5)}),
+                new Order(new User("shengqi"), new List<Product>() {new Product("NoteBook", 2.5)}),
+                new Order(new User("yanpei"), new List<Product>() {new Product("NoteBook", 5.5)}),
+                new Order(new User("yanpei"), new List<Product>() {new Product("NoteBook", 20)}),
+                new Order(new User("yanpei"), new List<Product>() {new Product("Pencil", 13)}),
+            };
+
+            var totalCostForShengqi = new Calculator().GetTotalCostForUser("shengqi", orders);
+            Assert.Equal(10.0, totalCostForShengqi);
+            var totalCostForYanpei = new Calculator().GetTotalCostForUser("yanpei", orders);
+            Assert.Equal(38.5, totalCostForYanpei);
+        }
+    }
+}
---
+++ Refactoring/ChangeValueToReference/Order.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Refactoring.ChangeValueToReference
+{
+    public class Calculator
+    {
+        public double GetTotalCostForUser(string userName, List<Order> orders)
+        {
+            return orders.Where(order => string.Equals(order.User.Name, userName))
+                .SelectMany(order => order.Products)
+                .Aggregate(0.0, (total, next) => total + next.Price);
+        }
+    }
+
+    public class Order
+    {
+        public Order(User user, List<Product> products)
+        {
+            User = user;
+            Products = products;
+        }
+        public User User { get; set; }
+        public List<Product> Products { get; set; }
+    }
+
+    public class User
+    {
+        public User(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+
+    public class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+
+}
