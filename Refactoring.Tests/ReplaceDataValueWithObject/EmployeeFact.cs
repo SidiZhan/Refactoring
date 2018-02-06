685fb477fc240a5b98446ebedb8bbbaa73f93d50
lovelivesimply <1047723382@qq.com>
2018-02-06 17:35:14 +0800

    replace data value with object

---
+++ Refactoring.Tests/ReplaceDataValueWithObject/EmployeeFact.cs
@@ -0,0 +1,51 @@
+﻿using Refactoring.ReplaceDataValueWithObject;
+using Xunit;
+
+namespace Refactoring.Tests.ReplaceDataValueWithObject
+{
+
+    public class EmployeeFact
+    {
+        [Fact]
+        public void should_get_primary_email_prefix()
+        {
+            Employee employee = new Employee("first", "employer", 20000, "first@TW.com");
+            string primaryEmailPrefix = employee.GetPrimaryEmailPrefix();
+            Assert.Equal("first", primaryEmailPrefix);
+        }
+
+        [Fact]
+        public void should_get_primary_email_postfix()
+        {
+            Employee employee = new Employee("first", "employer", 20000, "first@TW.com");
+            string primaryEmailPostfix = employee.GetPrimaryEmailPostfix();
+            Assert.Equal("TW.com", primaryEmailPostfix);
+        }
+
+        [Fact]
+        public void should_get_company_info_from_primary_email()
+        {
+            Employee employee = new Employee("first", "employer", 20000, "first@TW.com");
+            string primaryEmailPostfix = employee.GetCompanyInfoFromPrimaryEmail();
+            Assert.Equal("TW", primaryEmailPostfix);
+        }
+
+        [Fact]
+        public void should_get_second_email_prefix()
+        {
+            Employee employee =
+                new Employee("first", "employer", 20000, "first@TW.com") {SecondEmail = "second@TW2.com"};
+            string secondEmailPrefix = employee.GetSecondEmailPrefix();
+            Assert.Equal("second", secondEmailPrefix);
+        }
+
+        [Fact]
+        public void should_get_second_email_postfix()
+        {
+            Employee employee =
+                new Employee("first", "employer", 20000, "first@TW.com") {SecondEmail = "second@TW2.com"};
+            string secondEmailPostfix = employee.GetSecondEmailPostfix();
+            Assert.Equal("TW2.com", secondEmailPostfix);
+        }
+    }
+}
---
+++ Refactoring/ReplaceDataValueWithObject/Employee.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Refactoring.ReplaceDataValueWithObject
+{
+    public class Employee
+    {
+        public string Name { get; }
+        public string Employer { get; }
+        public long Salary { get; }
+        public string PrimaryEmail { get; set;}
+        public string SecondEmail { get; set; }
+        public string DateOfBirth { get; set; }
+
+
+        public Employee(string name, string employer, long salary, string primaryEmail)
+        {
+            Name = name;
+            Employer = employer;
+            Salary = salary;
+            PrimaryEmail = primaryEmail;
+        }
+
+        public string GetPrimaryEmailPrefix()
+        {
+            return GetEmailPrefix(PrimaryEmail);
+        }
+
+        public string GetPrimaryEmailPostfix()
+        {
+            return GetEmailPostfix(PrimaryEmail);
+        }
+
+        public string GetCompanyInfoFromPrimaryEmail()
+        {
+            var domain = PrimaryEmail.Substring(PrimaryEmail.IndexOf("@", StringComparison.Ordinal) + 1);
+            if (domain.Contains(".")) 
+            {
+                return domain.Substring(0, domain.IndexOf(".", StringComparison.Ordinal));
+            }
+            return domain;
+        }
+
+        string GetEmailPrefix(string email)
+        {
+            return email.Substring(0, email.IndexOf("@", StringComparison.Ordinal));
+        }
+
+        public string GetSecondEmailPrefix()
+        {
+            return GetEmailPrefix(SecondEmail);
+        }
+
+        public string GetSecondEmailPostfix()
+        {
+            return GetEmailPostfix(SecondEmail);
+        }
+
+        string GetEmailPostfix(string email)
+        {
+            return email.Substring(email.IndexOf("@", StringComparison.Ordinal) + 1);
+        }
+    }
+}
