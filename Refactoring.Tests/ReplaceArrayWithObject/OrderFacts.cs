6e9c8e1c2d7ef1c19c5c77e93687f4f229af830d
pyan <pyan@thoughtworks.com>
2018-03-05 08:22:40 +0800

    [yan] #8.5 replace array with object init.

---
+++ Refactoring.Tests/ReplaceArrayWithObject/OrderFacts.cs
@@ -0,0 +1,16 @@
+﻿using Refactoring.ReplaceArrayWithObject;
+using Xunit;
+
+namespace Refactoring.Tests.ReplaceArrayWithObject
+{
+    public class OrderFacts
+    {
+        [Fact]
+        public void should_get_total_price()
+        {
+            var order = new Order("Notebook", "2.1", "2");
+            var totalPrice = order.GetTotalPrice();
+            Assert.Equal(4.2, totalPrice);
+        }
+    }
+}
---
+++ Refactoring/ReplaceArrayWithObject/Order.cs
@@ -0,0 +1,19 @@
+﻿namespace Refactoring.ReplaceArrayWithObject
+{
+    public class Order
+    {
+        readonly string[] product = new string[3];
+
+        public Order(string name, string price, string amount)
+        {
+            product[0] = name;
+            product[1] = price;
+            product[2] = amount;
+        }
+
+        public double GetTotalPrice()
+        {
+            return double.Parse(product[1]) * int.Parse(product[2]);
+        }
+    }
+}
