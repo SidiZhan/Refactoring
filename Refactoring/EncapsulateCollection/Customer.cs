a2ed543363ee96a8fcd28d3981fba147434fdd1c
mengfan <mengfan@kitty.null>
2018-03-11 21:57:19 +0800

    add customer & order

---
+++ Refactoring/EncapsulateCollection/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Refactoring.EncapsulateCollection
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+
+        public HashSet<Order> Orders { get; set; }
+
+        public Customer()
+        {
+            Orders = new HashSet<Order>();
+        }
+    }
+}
---
+++ Refactoring/EncapsulateCollection/Order.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Refactoring.EncapsulateCollection
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+
+        public double Money { get; set; }
+    }
+}
