3c0039c473800899d89e0a8e9b06876725d947eb
mengfan <mengfan@kitty.null>
2018-03-19 21:28:49 +0800

    add Replace Record with Data Class

---
+++ Refactoring.Tests/ReplaceRecordWithDataClass/CustomerFacts.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Refactoring.ReplaceRecordWithDataClass;
+using Xunit;
+
+namespace Refactoring.Tests.ReplaceRecordWithDataClass
+{
+    public class CustomerFacts
+    {
+        readonly CustomerCache cache;
+
+        public CustomerFacts()
+        {
+            cache = new CustomerCache();
+        }
+
+        [Fact]
+        void load_customers_from_cache()
+        {
+            var customers = cache.LoadAll().OrderBy(e => e["name"]).ToList();
+
+            Assert.Equal(2, customers.Count());
+            Assert.Equal("76EF4D19-7503-4449-90B1-5FB85ECB5CA3", customers[0]["id"]);
+            Assert.Equal("Kayla", customers[0]["name"]);
+            Assert.Equal("B4B14933-E8D0-4D77-A599-AAD44093E5C5", customers[1]["id"]);
+            Assert.Equal("Nancy", customers[1]["name"]);
+        }
+    }
+}
---
+++ Refactoring/ReplaceRecordWithDataClass/CustomerCache.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Refactoring.ReplaceRecordWithDataClass
+{
+    public class CustomerCache
+    {
+        readonly HashSet<Customer> cache;
+
+        public CustomerCache()
+        {
+            cache = new HashSet<Customer>
+            {
+                new Customer
+                {
+                    ["id"] = "76EF4D19-7503-4449-90B1-5FB85ECB5CA3",
+                    ["name"] = "Kayla"
+                },
+                new Customer
+                {
+                    ["id"] = "B4B14933-E8D0-4D77-A599-AAD44093E5C5",
+                    ["name"] = "Nancy"
+                }
+            };
+        }
+
+        public IList<Customer> LoadAll()
+        {
+            return cache.ToList();
+        }
+    }
+}
