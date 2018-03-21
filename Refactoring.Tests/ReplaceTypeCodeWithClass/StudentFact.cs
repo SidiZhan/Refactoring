c9ac1502594775dd29af3f56d68aa7bc68cb87d4
lovelivesimply <1047723382@qq.com>
2018-03-21 11:54:35 +0800

    [dandan] replace type code with class

---
+++ Refactoring.Tests/ReplaceTypeCodeWithClass/StudentFact.cs
@@ -0,0 +1,31 @@
+﻿using Refactoring.ReplaceTypeCodeWithClass;
+using Xunit;
+
+namespace Refactoring.Tests.ReplaceTypeCodeWithClass
+{
+    public class StudentFact
+    {
+        [Fact]
+        void should_return__active_after_create_new_student()
+        {
+            Student student = new Student("Lily", Student.Active);
+            Assert.Equal(Student.Active, student.status);
+        }
+
+        [Fact]
+        void should_return__deleted_after_call_archive()
+        {
+            Student student = new Student("Lily", Student.Active);
+            student.Archive();
+            Assert.Equal(Student.Deleted, student.status);
+        }
+
+        [Fact]
+        void should_return__sleep_after_call_sleep()
+        {
+            Student student = new Student("Lily", Student.Active);
+            student.Sleep();
+            Assert.Equal(Student.Sleepd, student.status);
+        }
+    }
+}
---
+++ Refactoring/ReplaceTypeCodeWithClass/Student.cs
@@ -0,0 +1,29 @@
+﻿namespace Refactoring.ReplaceTypeCodeWithClass
+{
+    public class Student
+    {
+        public static int Active = 0;
+        public static int Sleepd = 1;
+        public static int Deleted = 2;
+
+        public string name { get; }
+
+        public int status { get; set;}
+
+        public Student(string name, int status)
+        {
+            this.name = name;
+            this.status = status;
+        }
+
+        public void Archive()
+        {
+            status = Deleted;
+        }
+
+        public void Sleep()
+        {
+            status = Sleepd;
+        }
+    }
+}
