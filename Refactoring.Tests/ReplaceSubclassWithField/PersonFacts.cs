64e70f85744532bccb5eba402cd3a2dc108ef193
pyan <pyan@thoughtworks.com>
2018-03-28 08:08:30 +0800

    [yan] #n/a init replace subclass with field.

---
+++ Refactoring.Tests/ReplaceSubclassWithField/PersonFacts.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml.Schema;
+using Refactoring.ReplaceSubclassWithField;
+using Xunit;
+
+namespace Refactoring.Tests.ReplaceSubclassWithField
+{
+    public class PersonFacts
+    {
+        [Fact]
+        void should_get_isMale_and_code_for_male()
+        {
+            var male = new Male();
+            Assert.True(male.IsMale());
+            Assert.Equal('M', male.GetCode());
+        }
+
+        [Fact]
+        void should_get_isMale_and_code_for_female()
+        {
+            var female = new Female();
+            Assert.False(female.IsMale());
+            Assert.Equal('F', female.GetCode());
+        }
+    }
+}
---
+++ Refactoring/ReplaceSubclassWithField/Person.cs
@@ -0,0 +1,34 @@
+﻿namespace Refactoring.ReplaceSubclassWithField
+{
+    public abstract class Person
+    {
+        public abstract bool IsMale();
+        public abstract char GetCode();
+    }
+
+    public class Male: Person
+    {
+        public override bool IsMale()
+        {
+            return true;
+        }
+
+        public override char GetCode()
+        {
+            return 'M';
+        }
+    }
+
+    public class Female: Person
+    {
+        public override bool IsMale()
+        {
+            return false;
+        }
+
+        public override char GetCode()
+        {
+            return 'F';
+        }
+    }
+}
