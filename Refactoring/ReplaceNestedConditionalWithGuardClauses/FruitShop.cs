8a916c2e0734f708aef719c57f5a0cd6db0d701d
mengfan <mengfan@kitty.null>
2018-04-03 23:00:14 +0800

    add ReplaceNestedConditionalWithGuardClauses

---
+++ Refactoring/ReplaceNestedConditionalWithGuardClauses/FruitShop.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+
+namespace Refactoring.ReplaceNestedConditionalWithGuardClauses
+{
+    public class FruitShop
+    {
+        readonly FruitType[] fruitTypes;
+
+        public FruitShop(params FruitType[] fruitTypes)
+        {
+            this.fruitTypes = fruitTypes ?? new FruitType[] { };
+        }
+
+        public FruitType? WhatShouldBuy()
+        {
+            FruitType? type;
+
+            if (fruitTypes.Contains(FruitType.Pomelo))
+            {
+                type = FruitType.Pomelo;
+            }
+            else if (fruitTypes.Contains(FruitType.Grape))
+            {
+                type = FruitType.Grape;
+            }
+            else if (fruitTypes.Contains(FruitType.Orange))
+            {
+                type = FruitType.Orange;
+            }
+            else
+            {
+                type = null;
+            }
+
+            return type;
+        }
+    }
+}
