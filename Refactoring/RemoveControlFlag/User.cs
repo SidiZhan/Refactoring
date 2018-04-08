f032bf9aa524f3b1375471d03e4cdac2aba6a549
DiyuanZhang <diyzhang@thoughtworks.com>
2018-04-08 18:57:51 +0800

    [diyuan] #n/a Consolidate Duplicate Conditional Fragments & Remove Control Flag

---
+++ Refactoring/RemoveControlFlag/User.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Refactoring.RemoveControlFlag
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
