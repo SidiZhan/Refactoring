8f28dd3252b996b069dd704e21743515e91b6b85
pyan <pyan@thoughtworks.com>
2018-05-09 11:08:15 +0800

    [yan] #n/a 10.5 & 10.6

---
+++ Refactoring.Tests/ParameterizeMethod/EmployeeFact.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Refactoring.ParameterizeMethod;
+using Xunit;
+
+namespace Refactoring.Tests.ParameterizeMethod
+{
+    public class EmployeeFact
+    {
+        [Fact]
+        void should_get_employee_payroll_for_engineer()
+        {
+            var engineer = Employee.Create(EmployeeType.ENGINEER);
+            Assert.Equal(3000, engineer.GetPayroll());
+        }
+
+        [Fact]
+        void should_get_employee_payroll_for_salesman()
+        {
+            var salesman = Employee.Create(EmployeeType.SALESMAN);
+            Assert.Equal(23000, salesman.GetPayroll());
+        }
+
+        [Fact]
+        void should_get_employee_payroll_for_manager()
+        {
+            var manager = Employee.Create(EmployeeType.MANAGER);
+            Assert.Equal(503000, manager.GetPayroll());
+        }
+
+        [Fact]
+        void should_raise_ten_percent_salary()
+        {
+            var engineer = Employee.Create(EmployeeType.ENGINEER);
+
+            Assert.Equal(3000, engineer.GetPayroll());
+            engineer.twentyPercentRaise();
+            Assert.Equal(3600, engineer.GetPayroll());
+        }
+
+        [Fact]
+        void should_raise_five_percent_salary()
+        {
+            var engineer = Employee.Create(EmployeeType.ENGINEER);
+
+            Assert.Equal(3000, engineer.GetPayroll());
+            engineer.fivePercentRaise();
+            Assert.Equal(3150, engineer.GetPayroll());
+        }
+    }
+}
---
+++ Refactoring/ParameterizeMethod/Employee.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Refactoring.ParameterizeMethod
+{
+    public enum EmployeeType
+    {
+        ENGINEER = 0,
+        SALESMAN = 1,
+        MANAGER = 2
+    }
+
+    public class Employee
+    {
+        internal double baseSalary = 3000;
+        internal const int commission = 20000;
+        internal const int bonus = 500000;
+
+        public static Employee Create(EmployeeType type)
+        {
+            switch (type)
+            {
+                case EmployeeType.ENGINEER:
+                    return new Engineer();
+                case EmployeeType.SALESMAN:
+                    return new Salesman();
+                case EmployeeType.MANAGER:
+                    return new Manager();
+                default:
+                    throw new ArgumentException("Incorrect type code value");
+            }
+        }
+
+        public virtual double GetPayroll()
+        {
+            throw new NotSupportedException();
+        }
+
+        public void twentyPercentRaise() { baseSalary *= 1.2; }
+
+        public void fivePercentRaise() { baseSalary *= 1.05; }
+    }
+
+    class Engineer : Employee
+    {
+        public override double GetPayroll()
+        {
+            return baseSalary;
+        }
+    }
+
+    class Salesman : Employee
+    {
+        public override double GetPayroll()
+        {
+            return baseSalary + commission;
+        }
+    }
+
+    class Manager : Employee
+    {
+        public override double GetPayroll()
+        {
+            return baseSalary + bonus;
+        }
+    }
+
+}
