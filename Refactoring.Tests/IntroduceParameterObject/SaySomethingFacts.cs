a412c841d44c37ae2fdcba1b4356016f850c8834
mengfan <mengfan@kitty.null>
2018-05-14 10:54:41 +0800

    add introduce parameter object

---
+++ Refactoring.Tests/IntroduceParameterObject/SaySomethingFacts.cs
@@ -0,0 +1,20 @@
+﻿using Refactoring.IntroduceParameterObject;
+using Xunit;
+
+namespace Refactoring.Tests.IntroduceParameterObject
+{
+    public class SaySomethingFacts
+    {
+        [Fact]
+        void should_say_hello()
+        {
+            Assert.Equal("Hello, First Middle Last", SaySomething.Hello("First", "Middle", "Last"));
+        }
+
+        [Fact]
+        void should_say_hi()
+        {
+            Assert.Equal("Hi, First Middle Last", SaySomething.Hi("First", "Middle", "Last"));
+        }
+    }
+}
---
+++ Refactoring/IntroduceParameterObject/SaySomething.cs
@@ -0,0 +1,15 @@
+﻿namespace Refactoring.IntroduceParameterObject
+{
+    public static class SaySomething
+    {
+        public static string Hello(string first, string middle, string last)
+        {
+            return $"Hello, {first} {middle} {last}";
+        }
+
+        public static string Hi(string first, string middle, string last)
+        {
+            return $"Hi, {first} {middle} {last}";
+        }
+    }
+}
