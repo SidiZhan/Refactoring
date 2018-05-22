039ce1c7dac3f68a2970bc1b8e3277cc46ceac2f
sqzhang <sqzhang@thoughtworks.com>
2018-05-22 22:15:18 +0800

    [shengqi] #10.14 replace error code with exception.

---
+++ Refactoring.Tests/ReplaceErrorCodeWithException/ReplaceErrorCodeWithExceptionFact.cs
@@ -0,0 +1,31 @@
+﻿using Refactoring.ReplaceErrorCodeWithException;
+using Xunit;
+
+namespace Refactoring.Tests.ReplaceErrorCodeWithException
+{
+    public class ReplaceErrorCodeWithExceptionFact
+    {
+        [Fact]
+        public void Withdraw_money_from_account()
+        {
+            var account = new Account(1000);
+            var user = new User(account);
+
+            user.Withdraw(100);
+
+            Assert.Equal(900, user.CheckAccountBalance());
+        }
+
+
+        [Fact]
+        public void Withdraw_money_from_account_overdrawn()
+        {
+            var account = new Account(1000);
+            var user = new User(account);
+
+            user.Withdraw(1100);
+
+            Assert.Equal(1000, user.CheckAccountBalance());
+        }
+    }
+}
---
+++ Refactoring/ReplaceErrorCodeWithException/Account.cs
@@ -0,0 +1,59 @@
+﻿namespace Refactoring.ReplaceErrorCodeWithException
+{
+    public class User
+    {
+        readonly Account account;
+
+        public User(Account account)
+        {
+            this.account = account;
+        }
+
+        public double Withdraw(double amount)
+        {
+            var withdrawCode = account.Withdraw(amount);
+            if (withdrawCode == -1)
+            {
+                HandleOverDrawn();
+                return 0;
+            }
+
+            DoSomething();
+            return amount;
+        }
+
+        public double CheckAccountBalance()
+        {
+            return account.Balance;
+        }
+
+        void DoSomething()
+        {
+        }
+
+        void HandleOverDrawn()
+        {
+        }
+    }
+
+    public class Account
+    {
+        public double Balance { get; private set; }
+
+        public Account(double balance)
+        {
+            Balance = balance;
+        }
+
+        public int Withdraw(double amount)
+        {
+            if (amount > Balance)
+            {
+                return -1;
+            }
+
+            Balance -= amount;
+            return 0;
+        }
+    }
+}
