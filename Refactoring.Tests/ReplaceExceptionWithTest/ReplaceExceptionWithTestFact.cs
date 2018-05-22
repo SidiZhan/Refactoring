b15fd17b6a279df531945ce9f00614b39ee8f0a9
sqzhang <sqzhang@thoughtworks.com>
2018-05-22 23:10:36 +0800

    [shengqi] #10.15 replace exception with test.

---
+++ Refactoring.Tests/ReplaceExceptionWithTest/ReplaceExceptionWithTestFact.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Refactoring.ReplaceExceptionWithTest;
+using Xunit;
+
+namespace Refactoring.Tests.ReplaceExceptionWithTest
+{
+    public class ReplaceExceptionWithTestFact
+    {
+        [Fact]
+        public void Get_resource_from_resource_pool()
+        {
+            var resource1 = new Resource("Resource1");
+            var avaliable = new List<Resource>() {resource1};
+            var respurcePool = new RespurcePool(null, avaliable);
+
+            respurcePool.GetResource("Resource1");
+            Assert.Equal(1, respurcePool.GetAllocatedResourceCount());
+        }
+
+        [Fact]
+        public void Get_resource_from_resource_pool_when_has_no_avaliable()
+        {
+            var respurcePool = new RespurcePool();
+            Assert.Equal(0, respurcePool.GetAvaliableResourceCount());
+            Assert.Equal(0, respurcePool.GetAllocatedResourceCount());
+
+            respurcePool.GetResource("Resource1");
+            Assert.Equal(0, respurcePool.GetAvaliableResourceCount());
+            Assert.Equal(1, respurcePool.GetAllocatedResourceCount());
+        }
+    }
+}
---
+++ Refactoring/ReplaceExceptionWithTest/ResourcePool.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Refactoring.ReplaceExceptionWithTest
+{
+    public class RespurcePool
+    {
+        List<Resource> Avaliable { get;}
+        List<Resource> Allocated { get;}
+
+        public RespurcePool(List<Resource> allocated = null, List<Resource> avaliable = null)
+        {
+            Allocated = allocated?? new List<Resource>();
+            Avaliable = avaliable?? new List<Resource>();
+        }
+
+        public int GetAvaliableResourceCount()
+        {
+            return Avaliable.Count;
+        }
+
+
+        public int GetAllocatedResourceCount()
+        {
+            return Allocated.Count;
+        }
+
+        public Resource GetResource(string name)
+        {
+            Resource result;
+            try
+            {
+                result = Avaliable.First(a => a.Name == name);
+                Allocated.Add(result);
+                return result;
+            }
+            catch (InvalidOperationException)
+            {
+                result = new Resource();
+                Allocated.Add(result);
+                return result;
+            }
+        }
+    }
+
+    public class Resource
+    {
+        public string Name { get; }
+
+        public Resource(string name = null)
+        {
+            Name = name;
+        }
+    }
+}
