2b607dfe78543f94970beb870d410e52608ab52d
DiyuanZhang <diyzhang@thoughtworks.com>
2018-06-04 19:26:39 +0800

    [diyuan] #n/a add form template method case

---
+++ Refactoring.Tests/FormTemplateMethod/OrderFact.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Refactoring.FormTemplateMethod;
+using Xunit;
+
+namespace Refactoring.Tests.FormTemplateMethod
+{
+    public class OrderFact
+    {
+        readonly Order order;
+        public OrderFact()
+        {
+            var products = new List<Product>
+            {
+                new Product
+                {
+                    Name = "product 1",
+                    Price = 100
+                },
+                new Product
+                {
+                    Name = "product 2",
+                    Price = 200
+                }
+            };
+            order = new Order(products);
+        }
+
+        [Fact]
+        public void should_get_text_order()
+        {
+            string result = $"There are 2 product in order\n";
+            result += "The product list is :\n";
+            result += $"product 1 : 100\n";
+            result += $"product 2 : 200\n";
+            result += $"Total price is 300\n";
+            result += "Please pay in 1 hour";
+            Assert.Equal(result,order.GetTextOrder());
+        }
+
+        [Fact]
+        public void should_get_html_order()
+        {
+            string result = $"<h1>There are 2 product in order</h1><br/>";
+            result += "The product list is :<br/>";
+            result += "<p>product 1 : 100</p>";
+            result += "<p>product 2 : 200</p>";
+            result += "Total price is 300<br/>";
+            result += "<strong>Please pay in 1 hour</strong>";
+            Assert.Equal(result, order.GetHtmlOrder());
+        }
+    }
+}
---
+++ Refactoring/FormTemplateMethod/Order.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Refactoring.FormTemplateMethod
+{
+    public class Order
+    {
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public Order(List<Product> products)
+        {
+            Products = products;
+        }
+
+        public string GetTextOrder()
+        {
+            string result = $"There are {Products.Count} product in order\n";
+            result += "The product list is :\n";
+
+            foreach (var product in Products)
+            {
+                result += $"{product.Name} : {product.Price}\n";
+            }
+
+            result += $"Total price is {Products.Sum(p => p.Price)}\n";
+            result += "Please pay in 1 hour";
+            return result;
+        }
+
+        public string GetHtmlOrder()
+        {
+            string result = $"<h1>There are {Products.Count} product in order</h1><br/>";
+            result += "The product list is :<br/>";
+
+            foreach (var product in Products)
+            {
+                result += $"<p>{product.Name} : {product.Price}</p>";
+            }
+
+            result += $"Total price is {Products.Sum(p => p.Price)}<br/>";
+            result += "<strong>Please pay in 1 hour</strong>";
+            return result;
+        }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
