074cea581efa162e674a05fdf0f530354982f5d1
DiyuanZhang <diyzhang@thoughtworks.com>
2018-06-04 19:50:36 +0800

    [diyuan] #n/a step 3 - move up common method

--- Refactoring/FormTemplateMethod/OrderPrinter.cs
+++ Refactoring/FormTemplateMethod/OrderPrinter.cs
@@ -2,9 +2,7 @@
 
 namespace Refactoring.FormTemplateMethod
 {
-    public class OrderPrinter{}
-
-    public class TextOrderPrinter : OrderPrinter
+    public abstract class OrderPrinter
     {
         public string GetOrder(Order order)
         {
@@ -17,19 +15,26 @@ namespace Refactoring.FormTemplateMethod
             return result;
         }
 
-        string GetFooter(Order order)
+        protected abstract string GetHeader(Order order);
+        protected abstract string GetEachProduct(Product product);
+        protected abstract string GetFooter(Order order);
+    }
+
+    public class TextOrderPrinter : OrderPrinter
+    {
+        protected override string GetFooter(Order order)
         {
             string result = $"Total price is {order.Products.Sum(p => p.Price)}\n";
             result += "Please pay in 1 hour";
             return result;
         }
 
-        string GetEachProduct(Product product)
+        protected override string GetEachProduct(Product product)
         {
             return $"{product.Name} : {product.Price}\n";
         }
 
-        string GetHeader(Order order)
+        protected override string GetHeader(Order order)
         {
             string result = $"There are {order.Products.Count} product in order\n";
             result += "The product list is :\n";
@@ -39,30 +44,19 @@ namespace Refactoring.FormTemplateMethod
 
     public class HtmlOrderPrinter : OrderPrinter
     {
-        public string GetOrder(Order order)
-        {
-            string result = GetHeader(order);
-            foreach (var product in order.Products)
-            {
-                result += GetEachProduct(product);
-            }
-            result += GetFooter(order);
-            return result;
-        }
-
-        string GetFooter(Order order)
+        protected override string GetFooter(Order order)
         {
             string result = $"Total price is {order.Products.Sum(p => p.Price)}<br/>";
             result += "<strong>Please pay in 1 hour</strong>";
             return result;
         }
 
-        string GetEachProduct(Product product)
+        protected override string GetEachProduct(Product product)
         {
             return $"<p>{product.Name} : {product.Price}</p>";
         }
 
-        string GetHeader(Order order)
+        protected override string GetHeader(Order order)
         {
             string result = $"<h1>There are {order.Products.Count} product in order</h1><br/>";
             result += "The product list is :<br/>";
