06469b13407ddc2958f8d666bd8bb5863e3c101e
DiyuanZhang <diyzhang@thoughtworks.com>
2018-06-04 19:36:20 +0800

    [diyuan] #n/a first step - create new class and subclass

--- Refactoring/FormTemplateMethod/Order.cs
+++ Refactoring/FormTemplateMethod/Order.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 
 namespace Refactoring.FormTemplateMethod
 {
@@ -14,32 +13,12 @@ namespace Refactoring.FormTemplateMethod
 
         public string GetTextOrder()
         {
-            string result = $"There are {Products.Count} product in order\n";
-            result += "The product list is :\n";
-
-            foreach (var product in Products)
-            {
-                result += $"{product.Name} : {product.Price}\n";
-            }
-
-            result += $"Total price is {Products.Sum(p => p.Price)}\n";
-            result += "Please pay in 1 hour";
-            return result;
+            return new TextOrderPrinter().GetOrder(this);
         }
 
         public string GetHtmlOrder()
         {
-            string result = $"<h1>There are {Products.Count} product in order</h1><br/>";
-            result += "The product list is :<br/>";
-
-            foreach (var product in Products)
-            {
-                result += $"<p>{product.Name} : {product.Price}</p>";
-            }
-
-            result += $"Total price is {Products.Sum(p => p.Price)}<br/>";
-            result += "<strong>Please pay in 1 hour</strong>";
-            return result;
+            return new HtmlOrderPrinter().GetOrder(this);
         }
     }
 
---
+++ Refactoring/FormTemplateMethod/OrderPrinter.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+
+namespace Refactoring.FormTemplateMethod
+{
+    public class OrderPrinter{}
+
+    public class TextOrderPrinter : OrderPrinter
+    {
+        public string GetOrder(Order order)
+        {
+            string result = $"There are {order.Products.Count} product in order\n";
+            result += "The product list is :\n";
+
+            foreach (var product in order.Products)
+            {
+                result += $"{product.Name} : {product.Price}\n";
+            }
+
+            result += $"Total price is {order.Products.Sum(p => p.Price)}\n";
+            result += "Please pay in 1 hour";
+            return result;
+        }
+    }
+
+    public class HtmlOrderPrinter : OrderPrinter
+    {
+        public string GetOrder(Order order)
+        {
+            string result = $"<h1>There are {order.Products.Count} product in order</h1><br/>";
+            result += "The product list is :<br/>";
+
+            foreach (var product in order.Products)
+            {
+                result += $"<p>{product.Name} : {product.Price}</p>";
+            }
+
+            result += $"Total price is {order.Products.Sum(p => p.Price)}<br/>";
+            result += "<strong>Please pay in 1 hour</strong>";
+            return result;
+        }
+    }
+}
